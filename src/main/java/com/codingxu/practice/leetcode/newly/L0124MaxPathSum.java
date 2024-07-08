64e39ace7b1323bfba903dd45587f334345158f6
codingxu97 <codingxu@outlook.com>
2024-07-08 21:36:15 +0800

    修改L0124未通过测试用例的问题

--- src/main/java/com/codingxu/practice/leetcode/newly/L0124MaxPathSum.java
+++ src/main/java/com/codingxu/practice/leetcode/newly/L0124MaxPathSum.java
@@ -1,5 +1,24 @@
 package com.codingxu.practice.leetcode.newly;
 
+/**
+ * 二叉树中的 路径 被定义为一条节点序列，序列中每对相邻节点之间都存在一条边。同一个节点在一条路径序列中 至多出现一次 。该路径 至少包含一个 节点，且不一定经过根节点。
+ * 路径和 是路径中各节点值的总和。
+ * 给你一个二叉树的根节点 root ，返回其 最大路径和 。
+ * <p>
+ * 示例 1：
+ * 输入：root = [1,2,3]
+ * 输出：6
+ * 解释：最优路径是 2 -> 1 -> 3 ，路径和为 2 + 1 + 3 = 6
+ * <p>
+ * 示例 2：
+ * 输入：root = [-10,9,20,null,null,15,7]
+ * 输出：42
+ * 解释：最优路径是 15 -> 20 -> 7 ，路径和为 15 + 20 + 7 = 42
+ * <p>
+ * 提示：
+ * 树中节点数目范围是 [1, 3 * 104]
+ * -1000 <= Node.val <= 1000
+ */
 public class L0124MaxPathSum {
     public static void main(String[] args) {
         Integer[] a = new Integer[]{-10,9,20,null,null,15,7};
@@ -23,6 +42,13 @@ public class L0124MaxPathSum {
 
     private static Integer max = Integer.MIN_VALUE;
 
+    /**
+     * 时间复杂度：O(N)，其中 N 是二叉树中的节点个数。对每个节点访问不超过 2 次。
+     * 空间复杂度：O(N)，其中 N 是二叉树中的节点个数。空间复杂度主要取决于递归调用层数，最大层数等于二叉树的高度，最坏情况下，二叉树的高度等于二叉树中的节点个数。
+     *
+     * @param root 根节点
+     * @return 路径和的最大值
+     */
     public static int maxPathSum(TreeNode root) {
         maxSum(root);
         return max;
@@ -32,8 +58,8 @@ public class L0124MaxPathSum {
         if (node == null) {
             return 0;
         }
-        int leftMax = maxSum(node.left);
-        int rightMax = maxSum(node.right);
+        int leftMax = Math.max(0, maxSum(node.left));
+        int rightMax = Math.max(0, maxSum(node.right));
 
         int maxSum = leftMax + rightMax + node.val;
         max = Math.max(max, maxSum);
